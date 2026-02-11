d086cefb8e0bec94f767eb82836f99fa4491b8cd
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2026-02-11 11:08:37 +0530

    Settings Page

--- frontend/src/pages/SettingPage.tsx
+++ frontend/src/pages/SettingPage.tsx
@@ -1,9 +1,13 @@
+import Footer from "@/components/Footer"
+import Header from "@/components/Header"
 import SettingsPage from "@/components/Settings"
 
 const SettingPage = () => {
   return (
     <div>
+        <Header/>
         <SettingsPage/>
+        <Footer/>
     </div>
   )
 }
